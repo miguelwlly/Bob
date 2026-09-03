ca79b24d11d991ddd0ced2ba0fe6a5b9d32f80bc
miguelwlly <wllymiguel37@gmail.com>
2026-09-03 15:29:44 -0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-const db=require('../db'),enc=require('./crypto');
+const db=require('./db'),enc=require('./crypto');
 function publicSettings(){const s=db.readPaymentSettings();return{enabled:!!s.enabled,environment:s.environment||'test',publicKey:s.publicKey||'',hasAccessToken:!!s.accessTokenEncrypted,accessTokenMasked:s.accessTokenEncrypted?'••••••••••••'+(s.accessTokenLast4||''):null,hasWebhookSecret:!!s.webhookSecretEncrypted,webhookSecretMasked:s.webhookSecretEncrypted?'••••••••••••'+(s.webhookSecretLast4||''):null,lastTestStatus:s.lastTestStatus||null,lastTestAt:s.lastTestAt||null}}
 function getAccessToken(){const s=db.readPaymentSettings();return s.accessTokenEncrypted?enc.decrypt(s.accessTokenEncrypted):null}
 function getWebhookSecret(){const s=db.readPaymentSettings();return s.webhookSecretEncrypted?enc.decrypt(s.webhookSecretEncrypted):null}
