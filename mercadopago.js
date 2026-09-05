625e3fb3f97678ae983827d010a3a8ae25749f8c
miguelwlly <wllymiguel37@gmail.com>
2026-09-05 19:21:29 -0300

    Update mercadopago.js

--- mercadopago.js
+++ mercadopago.js
@@ -51,15 +51,7 @@ async function createPreference(order, token, urls) {
     },
     auto_return: 'approved',
     statement_descriptor: 'BOB BURGUER',
-  payment_methods: {
-  excluded_payment_types: [
-    { id: 'credit_card' },
-    { id: 'debit_card' },
-    { id: 'prepaid_card' },
-    { id: 'ticket' }
-  ]
-}
-};
+  };  
   if (urls.webhook) {
     body.notification_url = urls.webhook;
   }
